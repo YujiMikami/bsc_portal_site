629f4a178d229a8070b402dcc3ddefa87a0ef97f
YujiMikami <y.mik.bsc@gmail.com>
2025-07-28 16:41:22 +0900

    Add files via upload

---
+++ app/Models/Department.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Department extends Model
+{
+    use SoftDeletes;
+    protected $primaryKey = 'department_id'; // 主キーのカラム名
+    public $incrementing = false; // 自動採番OFFにする（重要！）
+
+    public function saveDepartment(Request $request)
+    {
+        $this->department_id = $request->input('department_id');
+        $this->department_name = $request->input('department_name');
+        $this->department_explanation = $request->input('department_explanation');
+        
+        $this->save();
+    }
+
+}
