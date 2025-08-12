4a6c15aa4db2e985076126cb0e4118aa4aad4b2f
三上　裕司 <y.mik.bsc@gmail.com>
2025-08-12 10:46:47 +0900

    no message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,25 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    ここに各通知やお知らせを載せます。<br><br>
-                
-                    更新内容：<br>
-                    　社員名簿<br>
-                    　登録・更新・詳細・削除機能を追加<br>
-                    　カラムの指定<br>
-                    　社員名簿のCSV出力<br>
-                    　各テーブルの骨組み作成<br>
-                    　レイアウトの変更<br>
-                    　各テーブル登録・更新・詳細・削除機能を追加<br>
-                    　パスワード変更<br>
-
-                    <br>
-                    実装予定：<br>
-                    　シフト表<br>
-                    　チャット機能<br>
-                    　有給申請<br>
-                    　交通費申請<br>
-
+                    BSCポータルサイトへようこそ。<br>
+                    ここでは、社内における申請・報告などを行えます。<br>
+                    現在実装されている機能は下記の通りです。<br>
+                    ・有給申請<br>
+                    ・安否報告<br>
+                    今後、随時機能を追加していきます。<br>
                 </div>
             </div>
         </div>
